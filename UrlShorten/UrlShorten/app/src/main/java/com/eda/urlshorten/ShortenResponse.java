d55123b16a192b1f5597d370f5b3280037de5bfd
edacdm <edacdm8@gmaail.com>
2024-11-27 16:38:50 +0300

    Location_Tracker_with_Android_Studio

---
+++ UrlShorten/UrlShorten/app/src/main/java/com/eda/urlshorten/ShortenResponse.java
@@ -0,0 +1,18 @@
+package com.eda.urlshorten;
+
+public class ShortenResponse {
+
+    private String link;
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+}
+
+
+
+
