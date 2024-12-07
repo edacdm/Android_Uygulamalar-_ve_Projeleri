38893df122816f852e694d4d4fa7e8ecf3be3c14
Eda Nur <98267282+edacdm@users.noreply.github.com>
2024-12-07 21:24:54 +0300

    Add files via upload

---
+++ SimpleSocialMedia/app/src/main/java/com/ali/simplesocialmedia/MainActivity.java
@@ -0,0 +1,69 @@
+package com.ali.simplesocialmedia;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.ali.simplesocialmedia.databinding.ActivityMainBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.ktx.Firebase;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        auth = FirebaseAuth.getInstance();
+
+
+    }
+    public void clik_signup(View view){
+        String email= binding.editEmail.getText().toString();
+        String password= binding.editPassword.getText().toString();
+
+        if(email.equals("") || password.equals("") ){
+            Toast.makeText(this, "Email or Password can not be Null", Toast.LENGTH_SHORT).show();
+        }
+
+        else {
+            auth.createUserWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    Intent intent = new Intent(MainActivity.this, TimeAktivate.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(MainActivity.this,e.getLocalizedMessage() , Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+    }
+
+}
